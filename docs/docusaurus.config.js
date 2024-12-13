084acbceb878db5a21331a6d976d52556051378c
rohandahale <rdahale@iaa.es>
2024-12-13 17:59:28 +0100

    Setup docs deploy workflow

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -22,8 +22,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'rohandahale', // Usually your GitHub org/user name.
+  projectName: 'ehteval', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
